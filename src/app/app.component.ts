0e83ca77dad54b169c7d67524c65f80ba86eb2ed
unknown <mutiyah.kem@gmail.com>
2020-12-12 00:14:23 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio';
-}
+
+  myImage : string = "assets/images/myPicture.jpg";
+  project1 : string = "assets/images/project1.jpeg";
+  project2 : string = "assets/images/project2.jpeg";
+  project3 : string = "assets/images/project3.jpeg";
+
+
+}   /* end of AppComponent class */
